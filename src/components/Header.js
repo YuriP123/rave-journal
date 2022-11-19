4f4ab26a93c28a99d3f017b2fe4e14a42f4c86c2
YuriP123 <miguel.pasamonte2@gmail.com>
2022-11-18 17:28:22 -0800

    created components

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import '../App.scss'
+export default function Header(){
+
+    return(
+        <div className="Header">
+            <div>
+                <img src="../images/dancer.png" width="30px"alt="bruh"></img>
+                <h2>Yuri's Rave Journal</h2>
+            </div>
+
+        </div>
+    )
+}
