c18c6664486263294c6167e4b7674dcc1b1ec0d3
YuriP123 <miguel.pasamonte2@gmail.com>
2022-11-18 23:19:29 -0800

    updated api

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        title: "Hard Summer Festival",
+        location: "San Bernardino, CA",
+        url: "https://www.hardsummer.com/",
+        startDate: "29 Jul, 2022",
+        endDate: "31 Jul, 2022",
+        description: "Hard is a national music festival, music cruise and concert brand founded in 2007. The event line-ups consist of alternative and electronic acts and emerging talents. The first Hard Music Festival was held on December 31, 2007 in Downtown Los Angeles.",
+        imageUrl: "https://d3vhc53cl8e8km.cloudfront.net/hello-staging/wp-content/uploads/sites/38/2022/01/20115611/HARD2021_0801_174617-9753_JGB-3_767x767.jpg"
+    },
+    {
+        title: "EDC Las Vegas 2022",
+        location: "Las Vegas, NV",
+        url: "https://lasvegas.electricdaisycarnival.com/",
+        startDate: "19 May, 2022",
+        endDate: "21 May, 2022",
+        description: "Electric Daisy Carnival, commonly known as EDC, is an electronic dance music festival organized by promoter and distributor Insomniac. The annual flagship event, EDC Las Vegas, is held in May at the Las Vegas Motor Speedway, and is currently the largest electronic dance music festival in North America.",
+        imageUrl: "https://edm.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTg0OTAyNTA2MDYzNjAzMTc1/edc-lv-2021-fire-works-brian-rapaport-photo_21.jpg"
+
+    },
+    {
+        title: "Countdown NYE",
+        location:"San Bernardino, CA",
+        url: "https://countdownnye.com/",
+        startDate: "31 Dec, 2021",
+        endDate: "1 Jan, 2022",
+        description: "Countdown NYE 2022 is the annual New Year’s Eve blowout done by Insomniac at NOS Events Center in San Bernardino. It's a rave with a storyline — aliens have returned to Raveland and officials have issued a public warning that they'll attack the people of Raveland at midnight.",
+        imageUrl: "https://edmidentity.com/wp-content/uploads/2020/01/Ivan-Meneses-for-Insomniac-Events-13.jpg"
+    }
+]
